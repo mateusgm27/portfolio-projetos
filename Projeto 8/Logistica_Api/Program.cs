f17ff4498953c78910f2ae1c7c508ffe7c73fa96
Mateus Gonçalves Melo <mateus270516@gmail.com>
2026-09-05 21:34:11 -0300

    Fix CORS policy configuration syntax

--- Projeto 8/Logistica_Api/Program.cs
+++ Projeto 8/Logistica_Api/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Configurar CORS para liberar o Vercel
 builder.Services.AddCors(options => {
     options.AddPolicy("AllowAll", policy => {
-        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+  policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
     });
 });
 
